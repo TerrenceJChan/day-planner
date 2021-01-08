5bd75a1b65271a3cb00f5115a9ccdf5fd5c96b16
Terrence Chan <terrencechan.writer@gmail.com>
2021-01-08 04:30:40 -0500

    Saves and loads data.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,17 @@
+// Displays current time
 $('#current-time').text(moment().format('MMMM Do YYYY'));
+
+// localStorage loader
+for (let i = 0; i < 24; i++) {
+    $('.text-input').eq(i).val(localStorage.getItem(JSON.stringify(i)));
+}
+
+// localStorage saver
+for (let i = 0; i < 24; i++) {
+    $('.save').eq(i).click(function () {
+        localStorage.setItem(JSON.stringify(i), JSON.stringify($('.text-input').eq(i).val()));
+    })
+}
+
+
+
